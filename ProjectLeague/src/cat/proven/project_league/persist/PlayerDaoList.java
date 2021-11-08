b9d8f84f04a35bfdb4430c9b5faf0643cc564d7c
Andres <alumne@A27-10.proven.local>
2021-11-08 09:02:49 +0100

    DAO created in player dao list and implementation of player dao interface

--- ProjectLeague/src/cat/proven/project_league/persist/PlayerDaoList.java
+++ ProjectLeague/src/cat/proven/project_league/persist/PlayerDaoList.java
@@ -5,10 +5,68 @@
  */
 package cat.proven.project_league.persist;
 
+import cat.proven.project_league.model.Player;
+import cat.proven.project_league.model.Team;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author alumne
  */
-public class PlayerDaoList {
+public class PlayerDaoList implements PlayerDaoInterface{
+    public static PlayerDaoList instance;
+    private final List<Player> data;
+    
+    public PlayerDaoList(){
+        data = new ArrayList<>();
+    }
+    
+    public static PlayerDaoList instance(){
+        if(instance == null){
+            instance = new PlayerDaoList();
+        }
+        return instance;
+    }
+    
+    @Override
+    public Player findPlayerById(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Player> findPlayerByFullName(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Player> findPlayersByTeam(Team team) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int addPlayer(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int modifyPlayer(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int removePlayer(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int enrolPlayerToTeam(Team team, Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int unenrolPlayerToTeam(Team team, Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
