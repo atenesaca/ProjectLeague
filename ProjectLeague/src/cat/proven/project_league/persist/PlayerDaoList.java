f8cf50852df2f8eac21a5cfb3a572bf60e50dec1
Andres <alumne@A27-10.proven.local>
2021-11-08 08:55:33 +0100

    Player Dao List created

---
+++ ProjectLeague/src/cat/proven/project_league/persist/PlayerDaoList.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.proven.project_league.persist;
+
+/**
+ *
+ * @author alumne
+ */
+public class PlayerDaoList {
+    
+}
